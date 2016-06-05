28cdc39c9c1afbdde6355eeacea927f780b225ba
alejandromp12 <alejandromp12.4@gmail.com>
2016-06-05 17:50:50 -0600

    Update registros_salida.v

--- registros_salida.v
+++ registros_salida.v
@@ -21,85 +21,148 @@
 module registros_salida( 
 	input Write_Strobe,
 	input [7:0] Out_Port,
-	input [7:0] Port_ID ,
-	input [2:0] rst , //de la maquina
+	input [7:0] Port_ID,
+	input rst , //de la maquina
+	input reset,
 	input clk,
 	output reg [7:0] Dir,
 	output reg [7:0] Dato,
+	output reg [3:0] direccion,
 	output reg contro_listo,
 	output reg contro_lee,
-	output reg contro_escribe
+	output reg contro_escribe,
+	output reg [2:0]bandera
     );
-	 
-	 // asigancion de las posiciones del bus de entrada
-	 wire rst_DIR, rst_DAT, rst_C_LI, rst_C_E, rst_C_LE;
-	 
-	 assign rst_C_LI=rst  [0];
-	 assign rst_C_E= rst  [1];
-	 assign rst_C_LE= rst [2];
-	 
+
 	 //logica del programa
-	 always @(posedge clk)
+	 always @(posedge clk )
+	 if (reset)
+		begin
+			contro_listo<=1'b0;
+			contro_escribe<=1'b0;
+			contro_lee<=1'b0;
+			Dir<=8'b00000000;
+			Dato<=8'b00000000;
+			direccion<=4'b0000;
+			bandera<=3'b0;
+			
+		end
+	else
+	begin
 		if (Write_Strobe)
 			begin
 				case (Port_ID)
 					8'b00000001   : //control_listo 
 						begin
-							if (rst_C_LI)//me lo da la maquina
+							if (rst)//me lo da la maquina
 								contro_listo<=1'b0;
 							else
 								begin
-									if (Out_Port==8'b00000001)
+									if (Out_Port==8'b00000001) 
+											begin
 											contro_listo<=1'b1;
+											contro_lee<=1'b0;
+											contro_escribe<=1'b0;
+											end
 									else 
+									begin
 											contro_listo<=1'b0;
+											contro_lee<=contro_lee;
+											contro_escribe<=contro_escribe;
+										end
 								end
 						end
 					8'b00000010   : 
 						begin
-							if (rst_C_E)//me lo da la maquina
+							if (rst)//me lo da la maquina
 								contro_escribe<=1'b0;
 							else
 								begin
 									if (Out_Port==8'b00000001)
+										begin
 											contro_escribe<=1'b1;
+											contro_lee<=1'b0;
+											contro_listo<=1'b0;
+										end
 									else 
+									begin
 											contro_escribe<=1'b0;
+											contro_lee<=contro_lee;
+											contro_listo<=contro_listo;
+									end
 								end
 						end
 					8'b00000011   : 
 						begin
-							if (rst_C_LE)//me lo da la maquina
+							if (rst)//me lo da la maquina
 								contro_lee<=1'b0;
 							else
 								begin
 									if (Out_Port==8'b00000001)
-											contro_lee<=1'b1;
+											begin
+												contro_lee<=1'b1;
+												contro_listo<=1'b0;
+												contro_escribe<=1'b0;
+											end
 									else 
-											contro_lee<=1'b0;
-								end
+											begin
+												contro_lee<=1'b0;
+												contro_listo<=contro_listo;
+												contro_escribe<=contro_escribe;
+											end								end
 						end
-					8'b00000100   : Dir<=Out_Port; 
+					8'b00000100   : Dato<=Out_Port; 
 					
-					8'b00000101   : Dato<=Out_Port; 
-
+					8'b00000101   : begin
+					
+											Dir<=Out_Port; 
+											
+											if(Out_Port==8'hf1)
+												bandera<=3'b1;
+											else
+												bandera<=3'b0;
+											
+										 end
+					8'b00000110   : 
+										begin
+											case(Out_Port)
+												8'b00000000: direccion<=4'b0000;
+												8'b00000001: direccion<=4'b0001;
+												8'b00000010: direccion<=4'b0010;
+												8'b00000011: direccion<=4'b0011;
+												8'b00000100: direccion<=4'b0100;
+												8'b00000101: direccion<=4'b0101;
+												8'b00000110: direccion<=4'b0110;
+												8'b00000111: direccion<=4'b0111;
+												8'b00001000: direccion<=4'b1000;
+												default: direccion<=direccion;
+											endcase
+										end
 					default :
 							begin
-								
+								contro_listo<=contro_listo;
+								contro_lee<=contro_lee;
+								contro_escribe<=contro_escribe;
+								direccion<=direccion;
+								Dir<=Dir;
+								Dato<=Dato;
+								bandera<=bandera;
 							end
 				endcase
 			end
 		else
-			case(rst)
-				3'b001 : contro_listo<=  1'b0;
-				3'b010 : contro_lee <=   1'b0;
-				3'b011 : contro_escribe<=1'b0;
-				default: 
-					begin
-						contro_listo<=  1'b1;
-						contro_lee <=   1'b1;
-						contro_escribe<=1'b1;
-					end
-			endcase
-
+			if (rst)
+				begin
+					contro_listo<=  1'b0;
+					contro_lee <=   1'b0;
+					contro_escribe<=1'b0;
+				end
+			else
+				begin
+					contro_listo<=contro_listo;
+					contro_lee <=contro_lee;
+					contro_escribe<=contro_escribe;
+				end
+	end
 endmodule
+
